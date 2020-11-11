e7dc7a9c0526e3016be8dfd137bb84df1e61cd35
Camila Valentim <camilaifpb@hotmail.com>
2020-11-11 20:42:40 -0300

    header

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,12 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+      const { movies } = props;
     return (<div>
-        {this.props.movies[0]};
+      {movies.map((movie) => <MovieCard key = {movie.title}/>)};
     </div>
     );
   }
