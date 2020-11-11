3be4cfdbc153e3ca7cf91d0db12b40464715fb66
Camila Valentim <camilaifpb@hotmail.com>
2020-11-11 20:49:33 -0300

    header text

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,1 @@
 // implement MovieList component here
-/*import React from 'react';
-import MovieCard from './MovieCard';
-
-class MovieList extends React.Component {
-  render() {
-      const { movies } = props;
-    return (<div>
-      {movies.map((movie) => <MovieCard key = {movie.title}/>)};
-    </div>
-    );
-  }
-}
-
-export default MovieList;*/
