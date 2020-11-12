6fff4c2d68bdcc427b21e73783fb41ecc2369436
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-11 21:24:00 -0300

    Commit: 0.0.4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     const movies = this.props;
-    return <div>{movies}</div>;
+    return movies.map((movie) => <MovieCard key={movie.title} movie={movie} />);
   }
 }
 
