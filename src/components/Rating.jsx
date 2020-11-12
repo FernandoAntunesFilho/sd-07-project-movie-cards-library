21ca2cff1f94b0b9a12014c473c850cdb285f627
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-12 00:30:17 -0300

    Commit: 0.1.6

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,9 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return <div>teste</div>;
+  }
+}
+
+export default Rating;
