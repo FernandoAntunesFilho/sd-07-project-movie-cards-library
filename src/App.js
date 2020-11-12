3e5a2b72a9d95da450772d4a4632c831f96d4531
barrrosomarcio <barrrosomarcio@gmail.com>
2020-11-12 16:43:41 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
   return (
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      <Header/>
+      <Header />
     </div>
-    
   );
 }
 
