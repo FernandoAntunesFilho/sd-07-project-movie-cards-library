8088c4f2f74fba35f09dadfa7cddc69462c4a227
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-11 22:00:48 -0300

    Commit: 0.0.7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
@@ -14,4 +15,10 @@ class MovieCard extends Component {
   }
 }
 
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+
+};
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,11 @@ import MovieCard from './MovieCard';
 class MovieList extends Component {
   render() {
     const movies = this.props;
-    return movies.map((movie) => <MovieCard key={movie.title} movie={movie} />);
+    return (
+      <div>
+        {movies.map((movie) => (<MovieCard key={movie.title} movie={movie} />))}
+      </div>
+    );
   }
 }
 
