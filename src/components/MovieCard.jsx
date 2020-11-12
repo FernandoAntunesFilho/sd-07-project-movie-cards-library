ba1321cb52b39c411258268c674c7894354cdd43
wellington <profwellington17@gmail.com>
2020-11-12 17:12:31 -0300

    fix Requisitos 7-10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ class MovieCard extends React.Component {
     const { title, imagePath, subtitle, storyline, rating } = this.props.movie;
     return (
       <div>
-        <img src={imagePath} alt='Capa do filme'/>
+        <img src={imagePath} alt="Capa do filme" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
