6a9eff1289865186ac65ccb880e6516b9fb45164
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-11-12 15:45:16 -0300

    teste codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,35 @@
-// implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+
+class MovieCard extends React.Component {
+render() {
+   const {title,subtitle,storyline,rating,imagePath} = this.props.movie;
+    return(<div className="MovieCard">
+         <img className="movie-card-image" src={imagePath} alt="bla" />
+         <div className="movie-card-body">
+         <h4>{title}</h4>
+         <h5>{subtitle}</h5>
+         <p>{storyline}</p>
+      <div>
+          < Rating rating={rating}/>
+      </div>
+      </div>
+     </div>)
+}
+}
+MovieCard.defaultProps = { movie: {} };
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
+
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+
+
+
+class MovieList extends React.Component {
+    
+ render(){
+    const {movies} = this.props;
+    return( <div>
+
+       { movies.map(movie => <div > 
+       <MovieCard movie={movie} key={movie.title}/>
+       </div>) }
+    </div>)
+
+ }
+
+
+}
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
+export default MovieList;
