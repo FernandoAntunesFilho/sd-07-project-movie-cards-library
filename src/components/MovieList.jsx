0ac962526d1944d219048204358a8ca5f82cb539
Leonardo <lecastro16@gmail.com>
2020-11-12 16:14:42 -0300

    requisitos 1 ao 3 v3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import propTypes from "prop-types";
 
 class MovieList extends React.Component {
   render() {
     return <div>{this.props.movies}</div>;
   }
 }
+
+MovieList.propTypes = {
+  movies: propTypes.arrayOf(propTypes.object).isRequired,
+};
 
 export default MovieList;
