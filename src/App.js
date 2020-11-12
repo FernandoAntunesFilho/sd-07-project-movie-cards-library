d304765a1db64d3482b1a3f55f1fc8e957c003d3
barrrosomarcio <barrrosomarcio@gmail.com>
2020-11-12 17:51:47 -0300

    correcao cc

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import movies from './data';
+import MoviesArray from './data';
 
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
-      <MovieList movies={movies} />
+      <MovieList movies={MoviesArray} />
     </div>
   );
 }
-
 
 export default App;
