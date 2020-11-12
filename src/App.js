c9bc6b56581adb1c342c4d35a7b74ffe63b7ee1b
Camila Valentim <camilaifpb@hotmail.com>
2020-11-12 19:29:34 -0300

    array movie

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import MovieCard from './components/MovieCard';
 import MovieList from './components/MovieList';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MovieCard />
-      <MovieList />
+      <MovieList movies={data}/>
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,16 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
+
+
 
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props.movies;
+    const { movies } = this.props;
     return (
       <div>
-         {movies.map(movies)}
+         {movies.map((movie) => (<MovieCard movie={movie} /> ))}
       </div>
     );
   }
