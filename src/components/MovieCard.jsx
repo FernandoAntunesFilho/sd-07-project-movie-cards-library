c3d3bebc5f5ea52321a17824fae452f704621c95
richardfazeredo <richardcompletetour@gmail.com>
2020-11-12 03:35:48 -0300

    Requirement 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 
 class MovieCard extends Component {
   render() {
-    const { imagePath } = this.props.movie;
+    const { title, imagePath } = this.props.movie;
     return (
       <div className="movie-card" >
+
         <img src={imagePath} alt="Movie cover" />
+        <h4 className="movie-title">{title}</h4>
       </div>
     );
   }
