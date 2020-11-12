f0da0ca17a0d18ea041fa08a2c5ff987b6563c77
richardfazeredo <richardcompletetour@gmail.com>
2020-11-12 03:31:05 -0300

    Requirement 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 
 class MovieCard extends Component {
   render() {
+    const { imagePath } = this.props.movie;
     return (
       <div className="movie-card" >
-
+        <img src={imagePath} alt="Movie cover" />
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,6 @@ import MovieCard from './MovieCard';
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
-    console.log(movies);
     return (
       <div className="movie-list">
         { movies.map((movie) => <MovieCard key={movie.title} movie={movie} />) }
