cd8d76bd4e80817be5b441fae6539a91755f6837
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-12 00:25:18 -0300

    Commit: 0.1.4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline } = this.props.movie;
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div>
+        <img src={imagePath} alt="" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
@@ -20,6 +21,7 @@ MovieCard.propTypes = {
       title: PropTypes.string,
       subtitle: PropTypes.string,
       storyline: PropTypes.string,
+      imagePath: PropTypes.string,
     },
   ).isRequired,
 };
