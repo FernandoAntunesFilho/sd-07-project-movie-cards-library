da4837ae698af5db0192ecd0ee71fe4031a3c985
Camila Valentim <camilaifpb@hotmail.com>
2020-11-12 21:32:04 -0300

    code climate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,18 +4,18 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;  
-      return (
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
         <div>
           <div>
             <h4 className="movie-card-title">{title}</h4>
             <h5 className="movie-card-subtitle">{subtitle}</h5>
             <p className="movie-card-storyline">{storyline}</p>
-            <img className="movie-card-image" src={imagePath} />
+            <img className="movie-card-image" src={imagePath} alt="" />
           </div>
           <Rating className="movie-card-rating" rating={rating} />
-      </div>
-    );  
+        </div>
+      );
   }
 }
 
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
-    const {rating} = this.props;
+    const { rating } = this.props;
     return (
-      <div className='rating'>
+      <div className="rating">
         {rating}
       </div>
     );
