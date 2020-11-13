24d1f1b56d2c787710f03efa23838f648152f315
Leonardo <lecastro16@gmail.com>
2020-11-13 18:08:24 -0300

    exercicios 1 ao 17 v4

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,9 +5,7 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <header>
-        <p>{rating}</p>
-      </header>
+        <span className="rating">{rating}</span>
     );
   }
 }
