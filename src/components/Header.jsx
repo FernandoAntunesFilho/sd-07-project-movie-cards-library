f55c0cba69cbe4c4142b330d65508cee97ea4a9c
Demistocles <demi.treze@gmail.com>
2020-11-13 16:40:56 -0300

    enviando primeiro e segundo requisitos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-//gogo
+import React, { Component } from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="page-header">
+        <h1 className="page-title">Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
