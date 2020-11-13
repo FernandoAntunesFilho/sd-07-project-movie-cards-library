be79e4263dd1cd0d8b504b78720e664a46dd004f
Leonardo <lecastro16@gmail.com>
2020-11-13 17:48:47 -0300

    exercicios 1 ao 17 v1

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,7 +3,7 @@ import propTypes, { number } from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-      const {rating} = this.props;
+    const { rating } = this.props;
     return (
       <header>
         <h4>{rating}</h4>
