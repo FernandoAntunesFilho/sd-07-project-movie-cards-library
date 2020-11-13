0f297a0b0af2da033092e99b4052537aa23bbaeb
luc-zago <luc.zago@hotmail.com>
2020-11-13 18:57:28 -0300

    Adequação ao CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -14,6 +14,6 @@ class MovieList extends Component {
   }
 }
 
-MovieCard.propTypes = { movies: PropTypes.array.isRequired };
+MovieCard.propTypes = { movies: PropTypes.object.isRequired };
 
 export default MovieList;
