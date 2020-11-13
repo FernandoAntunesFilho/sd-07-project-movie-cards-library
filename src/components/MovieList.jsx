9a77339c7fa4cc3d26882144e437165a3db45f67
Leonardo <lecastro16@gmail.com>
2020-11-13 17:12:56 -0300

    exercicios 1 ao 8 v3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import MovieCard from ''./MovieCard';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
