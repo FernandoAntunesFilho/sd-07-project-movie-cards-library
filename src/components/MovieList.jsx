8b0fb7fb226e82b06a55f76b82b18735e3014a02
Camila Valentim <camilaifpb@hotmail.com>
2020-11-12 22:01:16 -0300

    propType

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -12,6 +13,7 @@ class MovieList extends React.Component {
     );
   }
 }
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 
 export default MovieList;
 
