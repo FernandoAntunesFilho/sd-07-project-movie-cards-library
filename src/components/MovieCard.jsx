283e74b7604402a3ee393a797b42f68ed2df6056
Camila Valentim <camilaifpb@hotmail.com>
2020-11-12 21:39:28 -0300

    code climate 20.3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-        <div>
+      <div>
           <div>
             <h4 className="movie-card-title">{title}</h4>
             <h5 className="movie-card-subtitle">{subtitle}</h5>
@@ -14,8 +14,8 @@ class MovieCard extends React.Component {
             <img className="movie-card-image" src={imagePath} alt="" />
           </div>
           <Rating className="movie-card-rating" rating={rating} />
-        </div>
-      );
+      </div>
+    );
   }
 }
 
