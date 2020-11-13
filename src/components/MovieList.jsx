29784b007152f833bfc23a30aad4741bb6fa8273
luc-zago <luc.zago@hotmail.com>
2020-11-13 16:17:50 -0300

    Finalizado projeto movie cards library

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard'
+
+class MovieList extends Component {
+  render() {
+
+    const movies = this.props.movies;
+
+    return (
+        <div className="movie-list">
+            {movies.map(movie => <MovieCard key={movie.title} movie={movie}/>)}
+        </div>
+    )
+  }
+}
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,11 @@
 // implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+
+    return (<h4 className="rating" >{this.props.rating}</h4>)
+  }
+}
+
+export default Rating;
