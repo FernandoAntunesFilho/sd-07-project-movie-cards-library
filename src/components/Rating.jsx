93f907f02bcc779e5ae5e11f0a7f6bccb5209091
Camila Valentim <camilaifpb@hotmail.com>
2020-11-12 21:20:30 -0300

    fiz nao sei como

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
+    const {rating} = this.props;
     return (
+      <div className='rating'>
+        {rating}
+      </div>
     );
   }
 }
