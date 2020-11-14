12bf1f05c603117a1c418dd1f95fe2174d819718
mayconfelipes <mayconfelipe.dev@gmail.com>
2020-11-14 00:41:44 -0300

    Todos os Requisitos on, será q passa no CodeClimate?. VQV Trybe!

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+import './MovieCard.css'
+
+class MovieCard extends React.Component {
+    render(){
+        const { imagePath, title, subtitle , storyline, rating} = this.props.movie;
+        return(
+            <div className="movieCard">
+                <img src={imagePath} alt={title} />
+                <h4>{title}</h4>
+                <h5>{subtitle}</h5>
+                <p>{storyline}</p>
+                <Rating rating={rating}/>
+            </div>
+        )
+    }
+}
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
 // implement Rating component here
+import React from 'react';
+import './Rating.css'
+
+class Rating extends React.Component{
+    render(){
+        return(
+        <div className="rating">{this.props.rating}</div>
+        )
+    }
+}
+
+export default Rating;
