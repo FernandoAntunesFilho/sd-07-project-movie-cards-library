81d59dd1ee25203449d8f70d03756dfd8b3a526b
Arnaelcio <arnaelcio.gomes@gmail.com>
2020-11-13 22:19:57 -0300

    iniciando o projeto. VAMOS COM TUDO

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,13 +5,11 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div className='movie-card-rating'>
-        <p className='rating'>{ rating }</p>
+      <div className="movie-card-rating">
+        <p className="rating">{ rating }</p>
       </div>
     );
   }
 }
-
-
 
 export default Rating;
