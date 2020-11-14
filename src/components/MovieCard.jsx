c69159378185d16bc1b9d259dede5c4295466f94
Alstent <rafaelasltent@gmail.com>
2020-11-14 21:26:10 +0100

    resolvendo code climate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating'
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={imagePath} alt="" />
@@ -17,8 +18,20 @@ class MovieCard extends React.Component {
           <Rating rating={rating} />
         </div>
       </div>
-    )
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape(
+    {
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      imagePath: PropTypes.string,
+      rating: PropTypes.number,
+    },
+  )
+};
 
 export default MovieCard;
