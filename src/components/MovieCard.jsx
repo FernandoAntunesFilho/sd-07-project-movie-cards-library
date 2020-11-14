3bf6b1e648acbf1e46d5e41e4b3526336bf0ed78
Alstent <rafaelasltent@gmail.com>
2020-11-14 20:12:27 +0100

    adicionando um header e props pro movieList e MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+export default MovieCard;
