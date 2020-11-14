f7bae5f3d2e2688377562b19fe0c282e7b63a896
Isabella Paz <bellapaz.iorp@gmail.com>
2020-11-13 22:37:16 -0300

    Project Movie Cards Library

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    const moviesList = movies.map((movie) => <MovieCard movie={movie} key={movie.title} />);
+    return (
+      <ul>
+        {moviesList}
+      </ul>
+    );
+  }
+}
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,12 @@
-// implement Rating component here
+
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return (
+      <div className="rating">{this.props.rating}</div>
+    );
+  }
+}
+
+export default Rating;
