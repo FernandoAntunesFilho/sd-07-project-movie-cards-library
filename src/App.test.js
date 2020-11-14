bb707d430f88b39bca65777b8e4dc5197591f8fa
paulohp7 <ph.pereira.7@gmail.com>
2020-11-14 20:11:27 -0300

    Eighth/Ninth/Tenth assignment with skips and codeclimate

--- src/App.test.js
+++ src/App.test.js
@@ -79,20 +79,20 @@ describe('<MovieCard /> component', () => {
     expect(wrapper.find('img').prop('src')).toEqual('images/movie_1');
   });
 
-  it.skip('renders the movie title inside an `h4` tag', () => {
+  it('renders the movie title inside an `h4` tag', () => {
     wrapper = shallow(<MovieCard movie={movie} />);
 
     expect(wrapper.find('h4').text()).toBe('Movie Title 1');
   });
 
-  it.skip('renders the movie subtitle inside an `h5` tag', () => {
+  it('renders the movie subtitle inside an `h5` tag', () => {
     wrapper = shallow(<MovieCard movie={movie} />);
 
     expect(wrapper.find('h5').text()).toBe('Movie Subtitle 1');
   });
 
 
-  it.skip('renders the movie storyline inside a `p` tag', () => {
+  it('renders the movie storyline inside a `p` tag', () => {
     wrapper = shallow(<MovieCard movie={movie} />);
 
     expect(wrapper.find('p').text()).toBe('Movie Storyline 1');
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,10 @@ class MovieCard extends React.Component {
     const movie = this.props.movie;
     return (
       <div>
+        <h4>{movie.title}</h4>
         <img src={movie.imagePath} alt="No source" />
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
       </div>
     );
   }
