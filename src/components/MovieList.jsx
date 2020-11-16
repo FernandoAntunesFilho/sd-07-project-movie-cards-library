9d5381fc5131a71fbbb6f8336f406f6d05e9b3de
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-11-16 15:04:45 -0400

    tentando chegar nos 100%

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import movies from '../data';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = [
-      {
-        title: 'Movie Title 1',
-        subtitle: 'Movie Subtitle 1',
-        storyline: 'Movie Storyline 1',
-        rating: 4.5,
-        imagePath: 'images/movie_1',
-      },
-      {
-        title: 'Movie Title 2',
-        subtitle: 'Movie Subtitle 2',
-        storyline: 'Movie Storyline 2',
-        rating: 4.5,
-        imagePath: 'images/movie_2',
-      },
-    ];
     return (
       <div>
         {movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
