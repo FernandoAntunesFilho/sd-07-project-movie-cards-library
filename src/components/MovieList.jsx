c99a75920a8254ce15c90edc00f196c817f56db1
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-11-16 14:31:58 -0400

    cc

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,6 +3,29 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const movies = [
+      {
+        title: 'Movie Title 1',
+        subtitle: 'Movie Subtitle 1',
+        storyline: 'Movie Storyline 1',
+        rating: 4.5,
+        imagePath: 'images/movie_1',
+      },
+      {
+        title: 'Movie Title 2',
+        subtitle: 'Movie Subtitle 2',
+        storyline: 'Movie Storyline 2',
+        rating: 4.5,
+        imagePath: 'images/movie_2',
+      },
+      {
+        title: 'Movie Title 3',
+        subtitle: 'Movie Subtitle 3',
+        storyline: 'Movie Storyline 3',
+        rating: 3,
+        imagePath: 'images/movie_3',
+      },
+    ];
     return (
       <div>
         {movies.map((movie, key) => <MovieCard movie={movie} key={movie.title} />)}
