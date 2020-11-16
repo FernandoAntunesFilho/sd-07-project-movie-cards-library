1c717059d200b2d2bdca496683f01eb6b3fccf3f
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-11-16 14:52:12 -0400

    sera?

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="page-title">
-          <div className="movie-card-header">
-            <Header />
-          </div>
-        </div>
-        <div className="movie-list">
-          <MovieList />
+function App () {
+  return (
+    <div className="App">
+      <div className="page-title">
+        <div className="movie-card-header">
+          <Header />
         </div>
       </div>
-    );
-  }
+      <div className="movie-list">
+        <MovieList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Rating from './Rating';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -7,23 +8,27 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <div className="movie-card-body">
-          <img className="movie-card-image" src={imagePath} alt="" />
-          <div className="movie-card-title">
-            <h4>{title}</h4>
-          </div>
-          <div className="movie-card-subtitle">
-            <h5>{subtitle}</h5>
-          </div>
-          <div className="movie-card-storyline">
-            <p>{storyline}</p>
-          </div>
-          <div className="movie-card-rating">
-            <Rating rating={rating} />
-          </div>
+          <img className="movie-card-image" src={imagePath} alt={title} />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={rating} /> 
         </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
