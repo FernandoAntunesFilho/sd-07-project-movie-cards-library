2eaf217b075dafdf96f402b2cfd8ef3ca61f8f8b
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-11-17 16:25:36 -0300

    cc

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,10 +6,10 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (<div>
-       { movies.map((movie) => <div> 
-       <MovieCard movie={movie} key={ movie.title }/>
-    </div>) }
-    </div>)
+      { movies.map((movie) => <div>
+         <MovieCard movie={movie} key={movie.title}/>
+       </div>) }
+    </div>);
   }
 }
 MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
-  return(  <span className="rating">{this.props.rating}</span>)
- }
+    return (<span className="rating">{this.props.rating}</span>);
+  }
 }
 export default Rating;
