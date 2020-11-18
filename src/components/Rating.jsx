e51bf64ce6af11e63041f1ec591d228891ef31e5
Eriquy Jhordan <eriquy@gmail.com>
2020-11-18 12:55:58 -0300

    Rating is ready

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,20 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <h1>Rating</h1>
+      <p>
+        Rating
+        {' '}
+        <span className="rating">{rating}</span>
+      </p>
     );
   }
 }
+
+Rating.propTypes = { rating: PropTypes.number.isRequired };
 
 export default Rating;
