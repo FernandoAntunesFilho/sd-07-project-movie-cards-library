0c61fa19f9c6a8927247df9519d02c5b8b87e2ed
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-11-20 19:52:23 -0300

    Reqs3e4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCardComponent extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default MovieCardComponent;
