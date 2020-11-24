2dc7335e5cf9c6945feaa5d536fabe71bf928c0b
André Ferreira <andrefpires1@gmail.com>
2020-11-24 01:11:58 -0300

    Criando component MovieList, importando data e testando acesso das props em Movie List

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    const data = this.props.data;
+    return (
+      <div>
+        <span>Nome: {data[0].title}</span>
+      </div>
+    )
+  }
+}
+
+export default MovieList;
