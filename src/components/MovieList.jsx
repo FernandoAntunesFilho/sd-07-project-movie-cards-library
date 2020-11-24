cb625b7e0f9ba8ca2279db74fcddbb511d93d631
André Ferreira <andrefpires1@gmail.com>
2020-11-24 03:43:41 -0300

    Adicionando chaves a cada MovieCard renderizado

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 class MovieList extends Component {
   render() {
     const movies = this.props.movies;
-    const movieCards = movies.map((dataMovie) => <MovieCard movie={dataMovie} />);
+    const movieCards = movies.map((dataMovie) => <MovieCard movie={dataMovie} key={dataMovie.title} />);
     return (
       <div className="movie-list">
         {movieCards}
