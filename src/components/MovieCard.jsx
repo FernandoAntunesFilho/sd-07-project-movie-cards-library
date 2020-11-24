9c744dbd0083991d44bab040f3733e682d291236
André Ferreira <andrefpires1@gmail.com>
2020-11-24 05:06:20 -0300

    Colocando classes css e arrumando elementos html/jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,10 +7,12 @@ class MovieCard extends Component {
     const movie = this.props.movie;
     return (
       <div className="movie-card">
-        <img src={movie.imagePath} alt={movie.title} />
-        <h4>{movie.title}</h4>
-        <h5>{movie.subtitle}</h5>
-        <p>{movie.storyline}</p>
+        <img src={movie.imagePath} alt={movie.title} className="movie-card-image" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{movie.title}</h4>
+          <h5 className='movie-card-subtitle'>{movie.subtitle}</h5>
+          <p className="movie-card-storyline">{movie.storyline}</p>
+        </div>
         <Rating rating={movie.rating} />
       </div>
     );
