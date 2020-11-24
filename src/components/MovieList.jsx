47109d6762bdfb876c195035cf8f4ff88509bcc0
André Ferreira <andrefpires1@gmail.com>
2020-11-24 01:35:04 -0300

    Criando component MovieList, importando data e testando acesso das props em Movie List | CC 4 - Importando propTypes em Movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
@@ -11,8 +12,8 @@ class MovieList extends Component {
   }
 }
 
-MovieList.propTypes = {
-  data = PropTypes.object
-};
-
 export default MovieList;
+
+MovieList.propTypes = {
+  data: PropTypes.object
+};
