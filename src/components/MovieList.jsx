252b69e8ab82e907410165a971f91ee9f470081c
MarceloBorges <melombm@hotmail.com>
2020-11-30 11:18:32 -0500

    correçao codeclimate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Movies from '../data';
-import MovieCard from './MovieCard'
+import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
   render() {
     const moviesList = this.props.moviesItens;
-    return(
+    return (
       <main>
         <div>
-          {Movies.map(moviesItens => <div>
-            <MovieCard  key={moviesItens.title} />
-          </div>)}
+          {Movies.map((moviesItens) => (
+            <div>
+              <MovieCard key={moviesItens.title} />
+            </div>
+          ))}
         </div>
       </main>
-    )
+    );
   }
 }
