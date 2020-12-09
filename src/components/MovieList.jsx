7eefa8b979de54c94ebb58bc9387ee3796c1894d
adrianoForcellini <adrianoforcellinipedretti@gmail.com>
2020-12-09 03:04:34 -0300

    fixIssues

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,6 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props.movies;
     return (
       movies.map(() => <MovieCard />)
     );
