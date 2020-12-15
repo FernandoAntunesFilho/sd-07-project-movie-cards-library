2a196f552909ac659815afac2aaa0a2f0af1e2b5
adrianoForcellini <adrianoforcellinipedretti@gmail.com>
2020-12-15 11:08:32 -0300

    Fix ISSUES

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Rating from "./Rating";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -11,7 +11,7 @@ class MovieCard extends React.Component {
         <h4>{movie.title}</h4>
         <h5>{movie.subtitle}</h5>
         <p>{movie.storyline}</p>
-        <Rating rating={movie.rating}/>
+        <Rating rating={movie.rating} />
       </div>
     );
   }
