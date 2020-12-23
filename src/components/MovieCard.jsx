d923ecbce6774ce7c8d8a3f6c0bca70d994bac2a
Alana Monteiro <lanaah.15@gmail.com>
2020-12-23 16:15:13 -0300

    commit inicial - Alana Monteiro

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+    render() {
+        return <div>
+            <h4>{ this.props.movieInfo.title }</h4>
+            <h5> { this.props.movieInfo.subtitle }</h5>
+            <p>{ this.props.movieInfo.storyline }</p>
+            <img alt={ this.props.movieInfo.title } src={this.props.movieInfo.imagePath} />
+        </div>
+    }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard'
+
+class MovieList extends React.Component {
+render () {
+    return (
+    <div>
+        <ul>
+          { this.props.object.map(movie => {
+           return (
+               <div>
+              <li> { movie.title } 
+              <MovieCard key={ movie.title } movieInfo={ movie } />
+              </li>
+              </div>
+           )
+          })
+          }
+        </ul>
+    </div>
+    )}
+}
+export default MovieList;
