a073f6a26556af191ea899bdfb9d6d02d4f41c60
Alana Monteiro <lanaah.15@gmail.com>
2020-12-23 19:30:12 -0300

    CC proptypes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 // import movies from '../data';
 import MovieCard from './MovieCard';
 // import MovieCard from './MovieCard'
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
     const { movieProp } = this.props;
     return (
       <div>
-        {movieProp.map((info) => {
+        {movieProp.map(info => {
           return (
             <MovieCard movieInfo={info} key={info.title} />
           );
@@ -18,5 +18,7 @@ class MovieList extends React.Component {
     );
   }
 }
+
+MovieList.propTypes = { movieProp: PropTypes.arrayOf(PropTypes.object).isRequired };
 
 export default MovieList;
