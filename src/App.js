8b999d68f8eb51a9863289e3b63a273b7a16ecef
lucasokazava <lucasokazava@gmail.com>
2021-01-07 11:01:45 +0000

    Adicionando referencias utilizadas para concluir o projeto

--- src/App.js
+++ src/App.js
@@ -14,3 +14,14 @@ function App() {
 }
 
 export default App;
+/* Nesse projeto eu tive ajuda do site do curso Trybe:
+  https://app.betrybe.com/course
+
+  Da Documentação do React:
+  https://pt-br.reactjs.org/docs/getting-started.html
+
+  E tive uma última ajuda em relação ao PropTypes porque o eslint não deixava passar... (mesmo o console
+    não dando warning):
+    https://github.com/yannickcr/eslint-plugin-react/issues/2079#issuecomment-537868142
+    Foi o comentário do @LukaGiorgadze.
+*/
