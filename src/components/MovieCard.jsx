160975605882035d206d31e89f90bdc71ed28c66
lucasokazava <lucasokazava@gmail.com>
2021-01-07 03:41:55 +0000

    Criando a prop movie e renderizando

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    return (
+      <div>
+        <img src={this.props.movie.imagePath} alt=""/>
+        <h4>{this.props.movie.title}</h4>
+        <h5>{this.props.movie.subtitle}</h5>
+        <p>{this.props.movie.storyline}</p>
+      </div>
+    );
   }
 }
 
