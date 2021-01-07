68212fba77bcc50dba31223c92687aee2a2125b9
lucasokazava <lucasokazava@gmail.com>
2021-01-07 09:43:12 +0000

    Definindo PropTypes && resolvendo CC

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={this.props.movie.imagePath} alt=""/>
+        <img className="movie-card-image" src={this.props.movie.imagePath} alt="" />
         <h4 className="movie-card-title">{this.props.movie.title}</h4>
         <h5 className="movie-card-subtitle">{this.props.movie.subtitle}</h5>
         <p className="movie-card-storyline">{this.props.movie.storyline}</p>
-        <div className="movie-card-rating"><Rating rating={this.props.movie.rating}/></div>
+        <div className="movie-card-rating"><Rating rating={this.props.movie.rating} /></div>
       </div>
     );
   }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.object
 }
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -11,6 +12,10 @@ class MovieList extends React.Component {
       </div>
     );
   }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.array
 }
 
 export default MovieList;
