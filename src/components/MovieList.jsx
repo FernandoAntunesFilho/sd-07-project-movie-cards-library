61aaa59620b7a778d2dfb00df2ff68b2b71d6936
lucasokazava <lucasokazava@gmail.com>
2021-01-07 06:56:43 +0000

    Renderizando dinamicamente o MovieCard && Rating

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    return (
+      <div>
+        {this.props.movies.map((movie) =>
+        (<MovieCard key={movie.title} movie={movie} />
+      ))}
+      </div>
+    );
   }
 }
 
