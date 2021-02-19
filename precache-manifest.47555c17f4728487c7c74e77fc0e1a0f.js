c64f8d7a52f7a0322e3978e6bbf41c24c7fcd738
dennerpl <denner.souzapl@gmail.com>
2021-02-19 14:25:41 -0300

    Updates

---
+++ precache-manifest.47555c17f4728487c7c74e77fc0e1a0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d308e99f75385fea9cbcb01994dd3876",
+    "url": "/movie-cards-library/index.html"
+  },
+  {
+    "revision": "f444b7bcec9e09e70859",
+    "url": "/movie-cards-library/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "ce03969a07b46381b5df",
+    "url": "/movie-cards-library/static/js/2.838aab56.chunk.js"
+  },
+  {
+    "revision": "f444b7bcec9e09e70859",
+    "url": "/movie-cards-library/static/js/main.d4ee2abf.chunk.js"
+  },
+  {
+    "revision": "ae17a1ff270632977288",
+    "url": "/movie-cards-library/static/js/runtime-main.c2f7205f.js"
+  }
+]);
